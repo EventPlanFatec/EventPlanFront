bd7c225f17a6994f792e6694fd2c4d0496d27645
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-10-02 16:19:22 -0300

    Atualização16

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <>
         <Routes>
-          <Route path="/" element={null} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} /> 
           <Route path="/home" element={<Home />} />
