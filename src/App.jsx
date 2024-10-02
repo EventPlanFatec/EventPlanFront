44328197ee1fc91c42cd08bb7ed9f614c3bbe8cb
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-10-02 18:43:42 -0300

    Teste11

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import './App.css';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Home from './pages/Home/Home';
-import Event from './pages/Event/Event';
-
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +16,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} /> 
           <Route path="/home" element={<Home />} /> 
-          <Route path="/event/:id" element={<Event />} />
         </Routes>
       </>
     </Router>
