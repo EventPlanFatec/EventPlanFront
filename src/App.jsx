fcd04f415648ce3e9aeafdd8f0dd25e4e1467717
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-10-02 15:41:13 -0300

    Atualização10

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,6 @@ import './App.css';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Home from './pages/Home/Home';
-import Event from './pages/Event/Event';
-import About from './pages/About/About';
-import Profile from './pages/Profile/Profile';
-import RecoverPass from './pages/RecoverPass/RecoverPass';
-import FAQ from './pages/FAQ/FAQ';
-import EventList from './pages/EventList/EventList.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,14 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={null} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> {/* Adiciona a rota para Register */}
+          <Route path="/register" element={<Register />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/event" element={<Event />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/recoverpass" element={<RecoverPass />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/eventlist" element={<EventList />} />
         </Routes>
       </>
     </Router>
