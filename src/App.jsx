45d0f1830474a3de8a0c8a0de4a73f2bd4946eb7
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-10-02 16:07:19 -0300

    Atualização14

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-import Home from './pages/home/home';
+import Home from './pages/Home/Home';
 
 function App() {
   const [count, setCount] = useState(0);
