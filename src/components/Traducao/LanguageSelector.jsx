44139298778b56362dc3306d0d1c2853c6867258
Clestxn <cleston.tonooka@fatec.sp.gov.br>
2024-10-02 17:57:42 -0300

    Atualização26

--- src/components/Traducao/LanguageSelector.jsx
+++ src/components/Traducao/LanguageSelector.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
 import { IntlProvider, FormattedMessage } from 'react-intl';
 import messages from './messages';
-import styles from './LanguageSelector.module.css';
+import { Box, Select, MenuItem, Typography } from '@mui/material';
 
 const LanguageSelector = () => {
     const [locale, setLocale] = useState('pt');
 
     const handleChangeLanguage = (e) => {
         setLocale(e.target.value);
-    }
+    };
 
     return (
         <IntlProvider locale={locale} messages={messages[locale]}>
-            <div className={styles.container}>
-                <select onChange={handleChangeLanguage}>
-                    <option value="pt">Português</option>
-                    <option value="en">English</option>
-                    <option value="es">Español</option>
-                    <option value="fr">Français</option>
-                </select>
-                <p>
+            <Box display="flex" flexDirection="column" alignItems="center">
+                <Select value={locale} onChange={handleChangeLanguage} variant="outlined">
+                    <MenuItem value="pt">Português</MenuItem>
+                    <MenuItem value="en">English</MenuItem>
+                    <MenuItem value="es">Español</MenuItem>
+                    <MenuItem value="fr">Français</MenuItem>
+                </Select>
+                <Typography variant="body1" mt={2}>
                     <FormattedMessage id="saudações" defaultMessage="Bem-vindo ao site EventPlan" />
-                </p>
-            </div>
+                </Typography>
+            </Box>
         </IntlProvider>
-    )
-}
+    );
+};
 
 export default LanguageSelector;
