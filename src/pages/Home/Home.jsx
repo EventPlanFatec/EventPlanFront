5b1145070a5cd2d153eae4e327f7fe2c896affcc
pedrobiondi23 <pedro.carvalho32@fatec.sp.gov.br>
2024-10-28 14:45:28 -0300

    Atualizações

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,18 +21,6 @@ const Home = () => {
 
   return (
     <Box>
-      <Box className={styles.videoContainer}>
-        <iframe
-          className={styles.video}
-          width="100%"
-          height="auto"
-          src="https://www.youtube.com/embed/YOUR_VIDEO_ID"
-          title="Trailer do Show do Roberto Carlos"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </Box>
       <Box className={styles.cardContainer}>
         {eventos.slice(0, 6).map(event => (
           <CardEvento key={event.id} event={event} />
