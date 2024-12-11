3f16dc27027425c82b640b06383c7131fb59316c
kayrocsilva <kayro.silva@fatec.sp.gov.br>
2024-12-11 01:17:58 -0300

    AboutUsCorrigido

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,8 @@ import rhian from "../../assets/rhian.jpeg";
 import Cleston from "../../assets/Cleston.jpeg";
 import Felipe from "../../assets/Felipe.jpeg";
 import Pedro from "../../assets/Pedro.jpeg";
+import Kayro from "../../assets/Kayro.jpeg";
+import Paulo from "../../assets/Paulo.jpeg";
 import './About.module.css';
 
 const About = () => {
@@ -23,6 +25,8 @@ const About = () => {
           { nome: "Cleston Kenji Maruyama Tonooka", img: Cleston },
           { nome: "Felipe Salgaço Moretti", img: Felipe },
           { nome: "Pedro Henrique Biondi de Carvalho", img: Pedro },
+          { nome: "Kayro César", img: Kayro },
+          { nome: "Paulo Selestrino", img: Paulo },
         ].map((membro, index) => (
           <Grid item xs={12} sm={6} md={3} key={index} textAlign="center">
             <Paper elevation={3} sx={{ padding: 1 }}>
